a611da401a5b0a85fd3bbe725478b9be1d5fda6c
mSounak2000 <m.sounak2000@gmail.com>
2021-07-08 04:21:20 +0530

    updated app.py

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# Food-Vision
-A Model to classify Food class based on Food101 Datasets
+# Food-Vision 🍜 📸
+
+Built an end-to-end **CNN Image Classification Model** which identifies the food in your image.
+
+I have used an pre-trained Image Classification Model, EfficientNetB0, previously pre-trained on ImageNet, that comes with Tensorflow and then retrained it on [Food 101 Dataset](https://www.kaggle.com/kmader/food41) dataset.
+
+## Fun Fact
+
+This model actually beats the [DeepFood](https://arxiv.org/abs/1606.05675) Paper's model which also trained on the same dataset.
+
+The Accuracy of DeepFood was **77.4%** and our models is **79%**. It is not much but, we take those.
+
+
+## How can you use it?
+---
+Finally after training the model, I have exported as `.h5` files and then integrated it with **Streamlit Web App**
+
+
+
+<div align="center">
+  <img src="./extras/app.jpg" height="540"/>
+ 
+</div>
+
+Once the app is loaded,
+
+1. Choose the input type. Whether you want to upload an image or enter URL of an image
+2. For upload an image option, choose image from your computer.
+3. For URL option, erase the existing url and enter your own url and press enter
+4. Once you have done the above, the model will predict the output and display the **Top-5 Predictions**
+
+
+
+### Installation
+
+1. Clone the repo
+   ```sh
+   git clone https://github.com/mSounak/Food-Vision
+   ```
+2. install `requirements.txt`
+   ```sh
+   pip install -r requirements.txt
+   ```
+3. run app.py
+   ```sh
+   streamlit run app.py
+   ```
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+
+
+
+<hr>
+
+<img alt="Python" src="https://img.shields.io/badge/python-%2314354C.svg?style=for-the-badge&logo=python&logoColor=white"/>
+
+<img alt="TensorFlow" src="https://img.shields.io/badge/TensorFlow-%23FF6F00.svg?style=for-the-badge&logo=TensorFlow&logoColor=white" />
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
--- app.py
+++ app.py
@@ -1,14 +1,14 @@
+from io import BytesIO
 import streamlit as st
 import tensorflow as tf
+import pandas as pd
 from PIL import Image
-import imageio
+import altair as alt
+import requests
 import os
 
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
-st.title("Food Image Classifier")
-
-
 @st.cache(allow_output_mutation=True)
 def load_model():
     model = tf.keras.models.load_model('models/FoodVision-EfficientNetB0.h5')
@@ -17,6 +17,24 @@ def load_model():
 with st.spinner('Loading Model into Memory......'):
     model = load_model()
 
+
+## Side bar
+with st.sidebar:
+    st.title('What is Food Vision?')
+    st.write("""
+    Food Vision is an end-to-end **CNN Image Classification Model** which identifies
+    over 100 food classes.
+    
+    It is based on pre-trained Image Classification Model `EfficienNetB0` which is 
+    trained on dataset `Food-101`.
+    """)
+    st.write("")
+    st.write("")
+    st.markdown(body='#### Choose Input type:')
+    st.write("")
+    method = st.sidebar.radio('', options=['Image', 'URL'])
+
+# Get the labels of the Food101
 labels=[]
 with open('meta/labels.txt', 'rb') as f:
     reader = f.read().splitlines()
@@ -24,10 +42,10 @@ with open('meta/labels.txt', 'rb') as f:
     for item in reader:
         labels.append(item.decode('utf-8'))
 
-
+# Prepare the data for prediction
 def load_prep_img(image, image_size=(224, 224), scale=False):
     
-    img = imageio.imread(image)
+    img = tf.image.decode_image(image, channels=3)
     img = tf.image.resize(img, size=image_size)
 
     if scale:
@@ -35,39 +53,73 @@ def load_prep_img(image, image_size=(224, 224), scale=False):
     else:
         return tf.expand_dims(img, axis=0)
 
-file = st.file_uploader('Please upload a food image', type=['png', 'jpeg', 'jpg'])
+# Predict the image
+def predicting(image, model):
+    image = load_prep_img(image)
+    preds = model.predict(image)
+    pred_class = labels[tf.argmax(preds[0])]
+    pred_conf = tf.reduce_max(preds[0])
+    top_5_i = sorted((preds.argsort())[0][-5:][::-1])
+    values = preds[0][top_5_i] * 100
+    classes = []
+    for x in range(5):
+        classes.append(labels[top_5_i[x]])
+    df = pd.DataFrame({"Top 5 Predictions": classes,
+                       "F1 Scores": values,
+                       'color': ['#EC5953', '#EC5953', '#EC5953', '#EC5953', '#EC5953']})
+    df = df.sort_values('F1 Scores')
+    return pred_class, pred_conf, df
 
-if file is not None:
-    
-    image = Image.open(file)
-    st.image(file, caption='Uploaded Image', use_column_width=True)
-
-    st.write("")
-    st.write("Predicted Class: ")
-    with st.spinner('Classifying........'):
-        img = load_prep_img(file)
-        probs = model.predict(img)
-        pred = labels[probs.argmax()]
-        st.write(pred)
-    
-        
 
 
+# Main Body
+st.title("Food Vision 🍜📸")
+st.header("Check your food photos!")
+st.write("To know more about this app, visit [**Github**](https://www.github.com/mSounak/Food-Vision)")
+if (method == 'Image'):
+    # For Image option
+    file = st.file_uploader('Please upload a food image', type=['png', 'jpeg', 'jpg'])
 
-    # st.write("""
-    #         # Food Classifier
-    #         """)
+    if file:
+        image = file.read()
+        st.image(file, caption='Uploaded Image', use_column_width=True)
 
-    # uploaded_file = st.file_uploader('Please upload a food image', type=['png', 'jpeg', 'jpg'])
+        st.write("")
+        st.write("Predicted Class: ")
+        with st.spinner('Classifying........'):
+            pred_class, pred_conf, df = predicting(image, model)
+            st.success(f'Prediction : {pred_class} | Confidence : {pred_conf*100:.2f}%')
+            st.write(alt.Chart(df).mark_bar().encode(
+                x='F1 Scores',
+                y=alt.X('Top 5 Predictions', sort=None),
+                color=alt.Color("color", scale=None),
+                text='F1 Scores'
+            ).properties(width=600, height=400))
+    else:
+        st.warning("Please upload an image")
+        st.stop()
 
-    # if uploaded_file:
-    #     st.image(uploaded_file, use_column_width=True)
-    #     read_img = ip.load_prep_img(uploaded_file)
+else:
+    # For URL option
+    url = st.text_input('Please enter image URL: ', value='https://kfoods.com/images1/newrecipeicon/club-sandwich_483.jpg')
 
-    #     pred,_ = ip.prediction(read_img)
+    if url:
+        content = requests.get(url).content
 
-    #     string = "This image is most likely: " + pred
-    #     st.success(string)
-    # else:
-    #     st.text("Please upload an image file")
+        image = Image.open(BytesIO(content))
+        st.image(image, caption='Uploaded Image', use_column_width=True)
 
+        st.write("")
+        st.write("Predicted Class: ")
+        with st.spinner('Classifying........'):
+            pred_class, pred_conf, df = predicting(content, model)
+            st.success(f'Prediction : {pred_class} | Confidence : {pred_conf*100:.2f}%')
+            st.write(alt.Chart(df).mark_bar().encode(
+                x='F1 Scores',
+                y=alt.X('Top 5 Predictions', sort=None),
+                color=alt.Color("color", scale=None),
+                text='F1 Scores'
+            ).properties(width=600, height=400))
+    else:
+        st.warning("Please enter an URL")
+        st.stop()
